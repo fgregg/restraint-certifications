6beb9e4b7101362901f0a76ad72e11d7d4b0a851
Forest Gregg <fgregg@datamade.us>
2023-09-21 12:13:06 -0400

    update data fetch

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -7,6 +7,7 @@ writer = csv.DictWriter(
     fieldnames=[
         "school",
         "school_id",
+        "student_count",
         "employeeFullName",
         "certExpirationDate",
         "position",
@@ -19,16 +20,21 @@ writer.writeheader()
 session = requests.Session()
 
 schools = session.get(
-    "https://api.cps.edu/schoolprofile/CPS/TypeaheadSchoolSearch"
+    "https://api.cps.edu/schoolprofile/CPS/AllSchoolProfiles"
 ).json()
 
 for school in schools:
     school_name = school["SchoolShortName"]
     school_id = school["SchoolID"]
+    student_count = school["StudentCount"]
     certifications = session.get(
         f"https://api.cps.edu/prto/api/CertifiedEmployees?schoolId={school_id}"
     ).json()
     for certification in certifications:
+        del certification['schoolId']
+        del certification['schoolShortName']
+        del certification['schoolLongName']
         certification["school"] = school_name
         certification["school_id"] = school_id
+        certification["student_count"] = student_count
         writer.writerow(certification)
